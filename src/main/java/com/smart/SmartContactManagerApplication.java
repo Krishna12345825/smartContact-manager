1f73281cc97f8fbcab4cb565f5178d9b64c078ef
KRISHNA KUMAR <krishnabhai117@gmail.com>
2022-11-13 01:35:30 +0530

    Merge branch 'master' of https://github.com/Krishna12345825/smartContact-manager.git

--- src/main/java/com/smart/SmartContactManagerApplication.java
+++ src/main/java/com/smart/SmartContactManagerApplication.java
@@ -8,6 +8,7 @@ public class SmartContactManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartContactManagerApplication.class, args);
+		System.out.println("Krishna kumar Yadav");
 	}
 
 }
